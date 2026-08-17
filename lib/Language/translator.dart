4c8726292623fbebb0e590a4c3e83e4321414dd3
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-17 11:50:18 +0600

    feat(i18n): add English and Arabic translation dictionaries

--- lib/Language/translator.dart
+++ lib/Language/translator.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
+import 'arabic.dart';
 import 'english.dart';
 
 class Translator extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
         'en_US': english,
+        'ar_SA': arabic,
       };
 }
